2aad30180755b183f4d604e06ad713de48c34c95
Oksana Petrosky <165575688+oksanapetrosky@users.noreply.github.com>
2025-01-10 10:57:36 -0500

    Update current-weather.jsx

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -12,7 +12,9 @@ const CurrentWeather = ({ data }) => {
           alt="weather"
           className="flex w-1/3 justify-center items-center gap-2 mt-4 mb-4"
         />
-        <p className="ftext-8xl font-bold text-white temperature">{Math.round(data.main.temp)}&deg;</p>
+        <p className="ftext-8xl font-bold text-white temperature">
+          {Math.round(data.main.temp)}&deg;
+        </p>
       </div>
       <div>
         <p className="text-white city">{data.city}</p>
@@ -21,30 +23,22 @@ const CurrentWeather = ({ data }) => {
           <p className="text-xl font-semibold">{time}</p>
         </div>
       </div>
-      {/* <div className="bottom">
-        <div className="details">
-          <div className="parameter-row">
-            <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">
-              {Math.round(data.main.feels_like)}°C
-            </span>
-          </div> */}
+
       <div className="w-full flex justify-center items-center mt-4 gap-4 text-white">
-  <div className="flex-1 text-center p-4 mt-2 font-bold bg-blue-600 shadow rounded-lg ">
-    <span>Wind Speed</span>
-    <span className="block font-normal">{data.wind.speed} mph</span>
-  </div>
-  <div className="flex-1 text-center p-4 mt-2 font-bold bg-green-600 rounded-lg">
-    <span>Humidity</span>
-    <span className="block font-normal">{data.main.humidity}%</span>
-  </div>
-</div>
-<hr className="mt-6 bg-slate-300 max-w-3xl shadow-lg"/>
-<p className="w-full p-4 flex justify-center items-center text-3xl font-semibold text-white">
-          {data.weather[0].description}
-        </p>
-    </div> 
-    // </div>
+        <div className="flex-1 text-center p-4 mt-2 font-bold bg-[#006BFF] opacity-1 shadow rounded-lg ">
+          <span>Wind Speed</span>
+          <span className="block font-normal">{data.wind.speed} mph</span>
+        </div>
+        <div className="flex-1 text-center p-4 mt-2 font-bold bg-[#006BFF] rounded-lg">
+          <span>Humidity</span>
+          <span className="block font-normal">{data.main.humidity}%</span>
+        </div>
+      </div>
+      <hr className="mt-6 bg-slate-300 max-w-3xl shadow-lg" />
+      <p className="w-full p-4 flex justify-center items-center text-3xl font-semibold text-white">
+        {data.weather[0].description}
+      </p>
+    </div>
   );
 };
 
